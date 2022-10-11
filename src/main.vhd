087141cfdeb9cf775a06672b5f972a4ccc364464
Elijah Ragozin <elijah.ragozin@gmail.com>
2022-10-10 22:07:27 -0400

    Update control unit, add tests

--- src/main.vhd
+++ src/main.vhd
@@ -11,16 +11,22 @@ port(
 	data    : inout std_logic_vector(0 to 7);
 	we_bar  : out std_logic;
 	mem_clk : out std_logic
-	
 );
 end entity CTRL;
 
 architecture behavioral of CTRL is
 
+signal we_bar_int : std_logic := '1';
+signal mem_clk_int : std_logic := '0';
+
+signal data_int : std_logic_vector(0 to 7);
+
 -- fetched data
 signal stored : std_logic_vector(0 to 15);
+
 alias stored0 is stored(0 to 7);
 alias stored1 is stored(8 to 15);
+
 alias cmd is stored(0 to 2);
 alias ra is stored(3 to 5);
 alias rb is stored(6 to 8);
@@ -28,19 +34,20 @@ alias rc is stored(13 to 15);
 alias i10 is stored(6 to 15);
 alias i7 is stored(9 to 15);
 
-signal alui0 : std_logic_vector(0 to 15);
-signal alui1 : std_logic_vector(0 to 15);
-signal aluo : std_logic_vector(0 to 15);
-
 -- registers
-signal r0 : std_logic_vector(0 to 15) := "0000000000000000";
-signal r1 : std_logic_vector(0 to 15);
-signal r2 : std_logic_vector(0 to 15);
-signal r3 : std_logic_vector(0 to 15);
-signal r4 : std_logic_vector(0 to 15);
-signal r5 : std_logic_vector(0 to 15);
-signal r6 : std_logic_vector(0 to 15);
-signal r7 : std_logic_vector(0 to 15);
+type registers is array(0 to 7) of std_logic_vector(0 to 15);
+signal r : registers := (
+	"0000000000000000",
+	"0000000000000000",
+	"0000000000000000",
+	"0000000000000000",
+	"0000000000000000",
+	"0000000000000000",
+	"0000000000000000",
+	"0000000000000000"
+);
+
+signal shadow : std_logic_vector(0 to 7);
 
 begin
 
@@ -48,41 +55,111 @@ main : process(clk) is
 
 variable pc : natural := 0;
 variable pcs : natural;
-constant divider : integer := 20800;
-variable cycle_counter : integer := 0;
-variable valid : boolean := false;
+variable fcycle : natural := 0;
 
 begin
-	if rising_edge(clk) and reset = '0' then
-		-- getting the current command
-		pcs := pc;
-		we_bar <= '1';
-		address <= std_logic_vector(to_unsigned(2 * pcs, address'length));
-		mem_clk <= '1';
-		stored0 <= data after 75 ns;
-		mem_clk <= '0' after 100 ns;
-		address <= std_logic_vector(to_unsigned(2 * pcs + 1, address'length)) after 125 ns;
-		mem_clk <= '1' after 125ns;
-		stored1 <= data after 200 ns;
-		mem_clk <= '0' after 225 ns;
-		valid := true;
-		pc := pc + 1;
+	if rising_edge(clk) and (reset = '0') then
+		if fcycle = 0 or fcycle = 1 then
+			pcs := pc;
+			we_bar_int <= '1';
+			mem_clk_int <= '1', '0' after 100 ns;
+			address <= std_logic_vector(to_unsigned(2 * pcs + fcycle, 16));
+		end if;
+		if fcycle = 0 then
+			-- fetch the first part
+			stored0 <= data after 50ns;
+		end if;
+		if fcycle = 1 then
+			-- fetch the second part
+			stored1 <= data after 50ns;
+			pc := pc + 1;
+		end if;
+		fcycle := fcycle + 1;
 	end if;
+
 	if falling_edge(clk) then
-		if valid then
-			valid := false;
+		if fcycle = 2 then
 			if cmd = "000" then
+				-- ADD
+				if ra /= "000" then
+					r(to_integer(unsigned(ra))) <= std_logic_vector(signed(r(to_integer(unsigned(rb)))) + signed(r(to_integer(unsigned(rc)))));
+				end if;
+				fcycle := 0;
 			elsif cmd = "001" then
+				-- ADDI
+				if ra /= "000" then
+					r(to_integer(unsigned(ra))) <= std_logic_vector(signed(r(to_integer(unsigned(rb)))) + signed(i7));
+				end if;
+				fcycle := 0;
 			elsif cmd = "010" then
+				-- NAND
+				if ra /= "000" then
+					r(to_integer(unsigned(ra))) <= not (r(to_integer(unsigned(rb))) and r(to_integer(unsigned(rc))));
+				end if;
+				fcycle := 0;
 			elsif cmd = "011" then
+				-- LUI
+				if ra /= "000" then
+					r(to_integer(unsigned(ra))) <= i10 & "000000";
+				end if;
+				fcycle := 0;
 			elsif cmd = "100" then
+				-- SW
+				we_bar_int <= '0';
+				address <= std_logic_vector(to_unsigned(2 * to_integer(signed(r(to_integer(unsigned(rb)))) + signed(i7)), 16));
+				mem_clk_int <= '1', '0' after 100 ns;
+				data_int <= r(to_integer(unsigned(ra)))(0 to 7) after 50 ns;
 			elsif cmd = "101" then
+				-- LW
+				if ra /= "000" then
+					we_bar_int <= '1';
+					address <= std_logic_vector(to_unsigned(2 * to_integer(signed(r(to_integer(unsigned(rb)))) + signed(i7)), 16));
+					mem_clk_int <= '1', '0' after 100 ns;
+					shadow <= data after 50 ns;
+				else
+					fcycle := 0;
+				end if;
 			elsif cmd = "110" then
+				-- BEQ
+				if r(to_integer(unsigned(ra))) = r(to_integer(unsigned(rb))) then
+					pc := pc + to_integer(signed(i7));
+				end if;
+				fcycle := 0;
 			elsif cmd = "111" then
+				-- JALR
+				if ra /= "000" then
+					r(to_integer(unsigned(ra))) <= std_logic_vector(to_unsigned(pc, 16));
+				end if;
+				pc := to_integer(unsigned(r(to_integer(unsigned(rb)))));
+				fcycle := 0;
+			end if;
+		end if;
+		if fcycle = 3 then
+			if cmd = "100" then
+				-- SW
+				we_bar_int <= '0';
+				address <= std_logic_vector(to_unsigned(2 * to_integer(signed(r(to_integer(unsigned(rb)))) + signed(i7)) + 1, 16));
+				mem_clk_int <= '1', '0' after 100 ns;
+				data_int <= r(to_integer(unsigned(ra)))(8 to 15) after 50 ns;
+				fcycle := 0;
+			elsif cmd = "101" then
+				-- LW
+				if ra /= "000" then
+					we_bar_int <= '1';
+					address <= std_logic_vector(to_unsigned(2 * to_integer(signed(r(to_integer(unsigned(rb)))) + signed(i7)) + 1, 16));
+					mem_clk_int <= '1', '0' after 100 ns;
+					r(to_integer(unsigned(ra))) <= shadow & data after 50 ns;
+				end if;
+				fcycle := 0;
 			end if;
 		end if;
 	end if;
 
 end process main;
+
+we_bar <= we_bar_int;
+mem_clk <= mem_clk_int;
+
+data <= data_int when (we_bar_int = '0' and mem_clk_int = '1' and reset = '0') else (others => 'Z');
 
 end architecture behavioral;
---
+++ src/test.vhd
@@ -0,0 +1,96 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity test_bench is
+end entity test_bench;
+
+architecture test_cpu of test_bench is
+	signal clk     : std_logic;
+	signal reset   : std_logic;
+
+	signal address : std_logic_vector(0 to 15);
+	signal data    : std_logic_vector(0 to 7);
+	signal we_bar  : std_logic;
+	signal mem_clk : std_logic;
+
+	signal data_int : std_logic_vector(0 to 7);
+
+begin
+	dut : entity work.CTRL(behavioral)
+	port map ( clk, reset, address, data, we_bar, mem_clk );
+
+stimulus : process is
+begin
+	reset <= '0';
+	clk <= '0', '1' after    25 ns, '0' after   250 ns, '1' after   500 ns, '0' after   750 ns,
+				 '1' after  1000 ns, '0' after  1250 ns, '1' after  1500 ns, '0' after  1750 ns,
+				 '1' after  2000 ns, '0' after  2250 ns, '1' after  2500 ns, '0' after  2750 ns,
+				 '1' after  3000 ns, '0' after  3250 ns, '1' after  3500 ns, '0' after  3750 ns,
+				 '1' after  4000 ns, '0' after  4250 ns, '1' after  4500 ns, '0' after  4750 ns,
+				 '1' after  5000 ns, '0' after  5250 ns, '1' after  5500 ns, '0' after  5750 ns,
+				 '1' after  6000 ns, '0' after  6250 ns, '1' after  6500 ns, '0' after  6750 ns,
+				 '1' after  7000 ns, '0' after  7250 ns, '1' after  7500 ns, '0' after  7750 ns,
+				 '1' after  8000 ns, '0' after  8250 ns, '1' after  8500 ns, '0' after  8750 ns,
+				 '1' after  9000 ns, '0' after  9250 ns, '1' after  9500 ns, '0' after  9750 ns,
+				 '1' after 10000 ns, '0' after 10250 ns, '1' after 10500 ns, '0' after 10750 ns,
+				 '1' after 11000 ns, '0' after 11250 ns, '1' after 11500 ns, '0' after 11750 ns;
+
+--	data <= "00100100" after   25 ns, "00000011" after  500 ns, -- ADDI R1 R0 3
+--			"00101000" after 1000 ns, "10000001" after 1500 ns, -- ADDI R2 R1 1
+--			"00001100" after 2000 ns, "10000010" after 2500 ns, -- ADD R3 R1 R2
+--			"01010001" after 3000 ns, "10000001" after 3500 ns; -- NAND R4 R3 R1 (3)
+--	data <= "01100111" after   25 ns, "11111111" after  500 ns; -- LUI R1
+--	data <= "00100100" after   25 ns, "00000001" after  500 ns, -- ADDI R1 R0 1
+--			"00001000" after 1000 ns, "00000000" after 1500 ns, -- ADD R2 R0 R0
+--			"11100101" after 2000 ns, "00000000" after 2500 ns; -- JARL R1 R2(SAVE R1)
+--	data <= "00100100" after   25 ns, "00000001" after  500 ns, -- ADDI R1 R0 1
+--			"00101000" after 1000 ns, "00000001" after 1500 ns, -- ADDI R2 R0 1
+--			"11100001" after 2000 ns, "00000000" after 2500 ns; -- JARL R0 R2
+--	data <= "00100100" after   25 ns, "00000011" after  500 ns, -- ADDI R1 R0 3
+--			"00101000" after 1000 ns, "00000001" after 1500 ns, -- ADDI R2 R0 1
+--			"11000101" after 2000 ns, "01000010" after 2500 ns; -- BEQ
+--	data <= "00100100" after   25 ns, "00000001" after  500 ns, -- ADDI R1 R0 1
+--			"00101000" after 1000 ns, "00000001" after 1500 ns, -- ADDI R2 R0 1
+--			"11000101" after 2000 ns, "01111110" after 2500 ns; -- BEQ
+--	data <= "00100100" after   25 ns, "00000011" after  500 ns, -- ADDI R1 R0 3
+--			"10101000" after 1000 ns, "10000001" after 1500 ns; -- LW R2 R1 1
+--	data <= "00100100" after   25 ns, "00000011" after  500 ns, -- ADDI R1 R0 3
+--			"00101000" after 1000 ns, "00000111" after 1500 ns, -- ADDI R2 R0 7
+--			"10001000" after 2000 ns, "11111111" after 2500 ns; -- SW R2 R1 -1 (address = 3-1=2 (4 and 5), data = 00000000 and 00000111)
+
+-- 0000: 0110010000000000    | LUI R1 0
+-- 0001: 0010010010000111    | ADDI R1 R1 7
+-- 0002: 1110000010000000    | JALR R0 R1
+-- 0003: 1000000000000000    | DATA 32768
+-- 0004: 1000000000000001    | DATA 32769
+-- 0005: 1000000000000010    | DATA 32770
+-- 0006: 1000000000000011    | DATA 32771
+-- 0007: 0010010000000101    | ADDI R1 R0 5
+-- 0008: 0111100000000000    | LUI R6 0
+-- 0009: 0011101100000011    | ADDI R6 R6 3
+-- 000a: 1011111100000000    | LW R7 R6 0
+-- 000b: 1000011110000000    | SW R1 R7 0
+-- 000c: 0111100000000000    | LUI R6 0
+-- 000d: 0011101100001100    | ADDI R6 R6 12
+-- 000e: 1111100000000000    | JALR R6 R0
+
+data <= 
+"01100100" after 25 ns, "00000000" after 500 ns,
+"00100100" after 1000 ns, "10000111" after 1500 ns,
+"11100000" after 2000 ns, "10000000" after 2500 ns,
+"00100100" after 3000 ns, "00000101" after 3500 ns,
+"01111000" after 4000 ns, "00000000" after 4500 ns,
+"00111011" after 5000 ns, "00000011" after 5500 ns,
+"10111111" after 6000 ns, "00000000" after 6500 ns,
+                     "10000000" after 6750 ns, "00000000" after 7250 ns,
+"10000111" after 7500 ns, "10000000" after 8000 ns,
+"01111000" after 8500 ns, "00000000" after 9000 ns,
+"00111011" after 9500 ns, "00001100" after 10000 ns,
+"11111000" after 10500 ns, "00000000" after 11000 ns;
+
+	wait;
+end process stimulus;
+
+-- data <= data_int; -- when (we_bar = '0' and mem_clk = '1') else (others => 'Z');
+
+end architecture test_cpu;
